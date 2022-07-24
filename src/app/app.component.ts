051c89c3aa81e3077102f792ded3fca123e7a3a1
Taras Peles <pelestaras@gmail.com>
2022-07-24 12:29:15 +0300

    added new template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [
-    '../assets/css/custom.css',
-    '../assets/css/bootstrap.min.css',
-    '../assets/css/fontawesome.css',
-    '../assets/css/fontawesome.min.css',
-    '../assets/css/slick-theme.css',
-    '../assets/css/slick-theme.min.css',
-    '../assets/css/slick.min.css',
-    '../assets/css/templatemo.css',
-    '../assets/css/templatemo.min.css',
+
 
 ]
 })
